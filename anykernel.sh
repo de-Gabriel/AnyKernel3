7e89060880aa301ac44993553bdb8c70a5b5ec15
AdityaKevin <adityakevin50@gmail.com>
2019-10-01 00:37:18 +0000

    anykernel: delicious

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=ProlougeKernel
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
